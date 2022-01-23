9588b3bc5c8707faf62deec94d66ab9e5e09e163
Soham Sarkar <soham.ss1@gmail.com>
2022-01-23 16:50:32 +0530

    search button + icons

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import {LeftBox, Flex, SearchButton} from './sidebar.css'
+import {MdMyLocation} from 'react-icons/md'
 
 export default function Sidebar() {
   return (
     <LeftBox>
-      <Flex>
-        <SearchButton />
+      <Flex variant="wide">
+        <SearchButton>Search for places</SearchButton>
+        <MdMyLocation className="location" size="22px" />
       </Flex>
     </LeftBox>
   )
--- src/components/sidebar/sidebar.css.ts
+++ src/components/sidebar/sidebar.css.ts
@@ -1,7 +1,8 @@
-import {styled} from '../../../stitches.config'
 import bgPath from '../../assets/cloud-bg.png'
+import {styled} from '../../../stitches.config'
+import {MdMyLocation} from 'react-icons/md'
 
-console.log(bgPath)
+MdMyLocation.toString = () => '.location'
 
 export const LeftBox = styled('div', {
   padding: '42px 46px 52px 46px',
@@ -17,12 +18,29 @@ export const LeftBox = styled('div', {
 export const Flex = styled('div', {
   display: 'flex',
   alignItems: 'center',
-  justifyContent: 'center',
+  variants: {
+    variant: {
+      wide: {
+        justifyContent: 'space-between',
+      },
+      center: {
+        justifyContent: 'center',
+      },
+    },
+  },
+  [`& ${MdMyLocation}`]: {
+    color: 'white',
+    border: '10px solid #6E707A',
+    borderRadius: '100%',
+  },
 })
 
 export const SearchButton = styled('button', {
   width: '161px',
   height: '40px',
   background: '#6E707A',
+  border: 'none',
+  color: 'white',
+  fontSize: '1.05em',
   boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
 })
