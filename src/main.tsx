a4259bf1374bd6c9a28bf094875fe1cdf0681109
Soham Sarkar <soham.ss1@gmail.com>
2022-01-23 12:05:27 +0530

    upgrade to react 18 and set yarn to v1

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import * as ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLDivElement,
+)
+
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
 )
